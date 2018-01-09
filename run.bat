448da5d69c4c19ea75e3f0cd5757d23d35fee8c5
Dmitriy Kononov <kondor_06@mail.ru>
2018-01-09 19:46:16 +0300

    'captcha'

--- run.bat
+++ run.bat
@@ -1,3 +1,11 @@
+rm -r "D:\USERS\kds\Documents\DEV\KononovDmitriy.github.io\*"
+ 
+git add *
+git commit -m 'captcha'
+
+copy "D:\USERS\kds\Documents\DEV\LOPOS-ID\build\*" "D:\USERS\kds\Documents\DEV\KononovDmitriy.github.io\"
+
 git add *
 git commit -m 'captcha'
 git push kds master
+

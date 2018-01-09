103d4e0470d9b072782308a9215fbc0fa47da96c
Кононов Дмитрий Сергеевич <kds@ad.atnn.ru>
2018-01-09 15:57:50 +0300

    222

---
+++ src/js/login/forgot.js
@@ -0,0 +1,59 @@
+import mainWindow from './main_login_window.js';
+import xhr from './../tools/xhr.js';
+import formForgot from './form_forgot.js';
+
+const emailVal = window.appSettings.forgotEmailValid;
+const urlApi = window.appSettings.forgotUrlApi;
+
+let callbackXhrSuccess = function (response) {
+  console.dir(response);
+
+  if (response.status === 400) {
+    alert(response.message);
+    mainWindow.firstScreen();
+  } else {
+    // показ ошибки
+    alert('Ошибка восстановления пароля');
+  }
+};
+
+let callbackXhrError = function (response) {
+  // показ ошибки
+  alert('error');
+};
+
+let validateForm = function (email) {
+
+  if (emailVal.test(email)) {
+    return true;
+  }
+  formForgot.setError('Введите корректный email');
+  return false;
+};
+
+let getRequestData = function (email) {
+
+  let requestData = `email=${email}`;
+  return {
+    url: urlApi,
+    metod: 'POST',
+    data: requestData,
+    callbackSuccess: callbackXhrSuccess,
+    callbackError: callbackXhrError
+  };
+};
+
+
+let submitForm = function (email) {
+  xhr.request = getRequestData(email);
+};
+
+export default {
+  submit(email) {
+
+    if (validateForm(email)) {
+      submitForm(email);
+    }
+
+  }
+};
---
+++ src/js/login/form_login.js
@@ -0,0 +1,47 @@
+import mainWindow from './main_login_window.js';
+import login from './login.js';
+
+const sectionLogin = document.querySelector('#sectionLogin');
+const loginForm = sectionLogin.querySelector('#loginForm');
+const loginButtonRegister = loginForm.querySelector('#loginButtonRegister');
+const loginButtonForgot = loginForm.querySelector('#loginButtonForgot');
+
+const inputFields = {
+  'login': loginForm.querySelector('#loginInputLogin'),
+  'password': loginForm.querySelector('#loginInputPassword')
+};
+
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  login.submit(inputFields.login.value, inputFields.password.value);
+});
+
+loginButtonRegister.addEventListener('click', function () {
+  mainWindow.register();
+});
+
+loginButtonForgot.addEventListener('click', function () {
+  mainWindow.forgot();
+});
+
+export default {
+
+  setError(target, msg) {
+    inputFields[target].setCustomValidity(msg);
+  },
+
+  show() {
+    sectionLogin.classList.remove('d-none');
+  },
+
+  hide() {
+    sectionLogin.classList.add('d-none');
+  },
+
+  reset() {
+    loginForm.reset();
+    inputFields.login.setCustomValidity('');
+    inputFields.password.setCustomValidity('');
+  }
+
+};

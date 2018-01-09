bcbc9a71c5b5b3175a2905b4c22f7a1f01d0c215
Кононов Дмитрий Сергеевич <kds@ad.atnn.ru>
2018-01-09 16:08:21 +0300

    3

--- src/js/tools/captcha.js
+++ src/js/tools/captcha.js
@@ -1,9 +1,19 @@
-window.captchaOnLoad = function () {
-  console.log('Капча загружена');
-};
-
 
 export default {
+  init() {
+
+    window.captchaOnLoad = function () {
+      console.log('Капча загружена');
+
+      grecaptcha.render('loginButtonSubmit', {
+            'sitekey' : '6Lct5j8UAAAAAJkVidQhkOxmBhI9uj28IQ5-Ltp9',
+            'callback' : captchaCallback
+          });
+
+    };
+
+  },
+
   captchaCallback() {
 
 
